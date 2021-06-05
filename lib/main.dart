37c481ee1174d580b7506b602bfcf9d62fcb07b0
puneetkukreja98 <puneetkukreja98@gmail.com>
2021-06-05 17:21:13 +0530

    adding question bank

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() {
   runApp(MyApp());
@@ -30,6 +31,43 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
+    Question(q: 'Some cats are actually allergic to humans', a: true),
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
+    Question(q: 'Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', a: true),
+    Question(q: 'It is illegal to pee in the Ocean in Portugal.', a: true),
+    Question(
+        q: 'No piece of square dry paper can be folded in half more than 7 times.',
+        a: false),
+    Question(
+        q: 'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        a: true),
+    Question(
+        q: 'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        a: false),
+    Question(
+        q: 'The total surface area of two human lungs is approximately 70 square metres.',
+        a: true),
+    Question(q: 'Google was originally called \"Backrub\".', a: true),
+    Question(
+        q: 'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        a: true),
+    Question(
+        q: 'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        a: true),
+  ];
+
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -42,7 +80,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go,',
+                questionBank[questionNumber].questionText!,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -57,7 +95,23 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(15.0),
             child: TextButton(
               onPressed: () {
-                // The use picked false
+                bool correctAnswer =
+                    questionBank[questionNumber].questionAnswer!;
+                if (correctAnswer == true) {
+                  print('User got it right');
+                } else {
+                  print('User got it wrong');
+                }
+                setState(() {
+                  questionNumber++;
+
+                  // scoreKeeper.add(
+                  //   Icon(
+                  //     Icons.check,
+                  //     color: Colors.green,
+                  //   ),
+                  // );
+                });
               },
               child: Text(
                 'True',
@@ -76,7 +130,22 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(15.0),
             child: TextButton(
               onPressed: () {
-                // The use picked false
+                bool correctAnswer =
+                    questionBank[questionNumber].questionAnswer!;
+                if (correctAnswer == false) {
+                  print('User got it right');
+                } else {
+                  print('User got it wrong');
+                }
+                setState(() {
+                  questionNumber++;
+                  // scoreKeeper.add(
+                  //   Icon(
+                  //     Icons.check,
+                  //     color: Colors.green,
+                  //   ),
+                  // );
+                });
               },
               child: Text(
                 'False',
@@ -89,6 +158,9 @@ class _QuizPageState extends State<QuizPage> {
                   backgroundColor: MaterialStateProperty.all(Colors.red)),
             ),
           ),
+        ),
+        Row(
+          children: scoreKeeper,
         ),
       ],
     );
